2d6ed79c5a1dba8864a1255a08b1c956b0b596ed
Dhruvit Desai <a1986442@adelaide.edu.au>
2026-09-17 15:01:23 +0930

    feat(listings): connect listing routes to API

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
+
 import authRoutes from "../modules/auth/auth.routes";
+import listingRoutes from "../modules/listings/listing.routes";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
+router.use("/listings", listingRoutes);
 
 export default router;
